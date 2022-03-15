1cd54944c5959747c2ea114a95112e4b3ebaf71c
Scareface <tektonikboy98@gmail.com>
2022-03-15 09:58:33 +0300

    Changed tests

--- js_vs_python_performance/main.py
+++ js_vs_python_performance/main.py
@@ -33,25 +33,61 @@ def quick_sort(arr):
         return arr
 
 
-# Test 1 add two numbers
-start = timeit.default_timer()
-for i in range(10000):
-    add_nums(1, 2)
-print(timeit.default_timer() - start)
+# Test 1 Add two numbers
+time_list_1 = []
+
+for _ in range(1000):
+    start = timeit.default_timer()
+
+    for _ in range(10000):
+        add_nums(1, 2)
+
+    exec_time = timeit.default_timer() - start
+    time_list_1.append(exec_time)
+
+print('Test 1:', max(time_list_1))
+
 
 # Test 2 Power the number
-start = timeit.default_timer()
-for i in range(10000):
-    pow_nums(123, 123)
-print(timeit.default_timer() - start)
+time_list_2 = []
+
+for _ in range(1000):
+    start = timeit.default_timer()
+
+    for _ in range(10000):
+        pow_nums(123, 123)
+
+    exec_time = timeit.default_timer() - start
+    time_list_2.append(exec_time)
+
+print('Test 2:', max(time_list_2))
+
 
 # Test 3 Generate array with random numbers from 0 to 10
-start = timeit.default_timer()
-generate_list()
-print(timeit.default_timer() - start)
+time_list_3 = []
+
+for _ in range(1000):
+    start = timeit.default_timer()
+
+    generate_list()
+
+    exec_time = timeit.default_timer() - start
+    time_list_3.append(exec_time)
+
+print('Test 3:', max(time_list_3))
+
 
 # Test 4 Sort array with random numbers from 0 to 10 with "Quick sort" algorithm
+time_list_4 = []
 random_num_list = generate_list()
-start = timeit.default_timer()
-quick_sort(random_num_list)
-print(timeit.default_timer() - start)
+
+for _ in range(1000):
+    start = timeit.default_timer()
+
+    quick_sort(random_num_list)
+
+    exec_time = timeit.default_timer() - start
+    time_list_4.append(exec_time)
+
+print('Test 4:', max(time_list_4))
+
--- js_vs_python_performance/script.js
+++ js_vs_python_performance/script.js
@@ -1,30 +1,79 @@
-randomNumList = generateList();
+// Test 1 Add two numbers
+let timeList1 = [];
 
-console.time('Func 1');
-for (let i=0; i <= 10000; i++) {
-    addNums(1, 2);
+for (let i=0; i <= 1000; i++) {
+    let start = performance.now();
+
+    for (let i = 0; i <= 10000; i++) {
+        addNums(1, 2);
+    }
+
+    let end = performance.now();
+    let total = end - start;
+    timeList1.push(total);
 }
-console.timeEnd('Func 1');
 
-console.time('Func 2');
-Math.pow(123, 123);
-console.timeEnd('Func 2');
+console.log(Math.max.apply(Math, timeList1));
 
-console.time('Func 3');
-generateList();
-console.timeEnd('Func 3');
 
-console.time('Func 4');
-quickSort(randomNumList);
-console.timeEnd('Func 4');
+// Test 2 Power the number
+let timeList2 = [];
+
+for (let i=0; i <= 1000; i++) {
+    let start = performance.now();
+
+    Math.pow(123, 123);
+
+    let end = performance.now();
+    let total = end - start;
+    timeList2.push(total);
+}
+
+console.log(Math.max.apply(Math, timeList2));
+
+
+// Test 3 Generate array with random numbers from 0 to 10
+let timeList3 = [];
+
+for (let i=0; i <= 1000; i++) {
+    let start = performance.now();
+
+    generateList();
+
+    let end = performance.now();
+    let total = end - start;
+    timeList3.push(total);
+}
+
+console.log(Math.max.apply(Math, timeList3));
+
+
+// Test 4 Sort array with random numbers from 0 to 10 with "Quick sort" algorithm
+let timeList4 = [];
+let randomNumList = generateList();
+
+for (let i=0; i <= 1000; i++) {
+    let start = performance.now();
+
+    quickSort(randomNumList);
+
+    let end = performance.now();
+    let total = end - start;
+    timeList4.push(total);
+}
+
+console.log(Math.max.apply(Math, timeList4));
+
 
 function addNums(a, b) {
     return a + b;
 }
 
+
 function generateList() {
     return Array.from({length: 10000}, () => Math.floor(Math.random() * 10));
 }
+
 
 function quickSort(arr) {
     if (arr.length < 2) return arr;
